90e7b08924bf82dbaaa39d06305086b1fcd3181d
Somanath1458 <37767811+Somanath1458@users.noreply.github.com>
2021-12-07 11:49:04 +0530

    update profile image

---
+++ index.js
@@ -0,0 +1,23 @@
+// import { animate } from "popmotion"
+
+// const animate = document.querySelector('.animate'))
+
+// popmotion.animate({
+//     from:{
+//         scale:.7
+//     },
+//     to:{
+//         scale:1
+//     },
+//     duration:1000
+// }).start(animate.set)
+
+
+// window.sr = ScrollReveal({duration:5000});
+// sr.reveal('.reveal');
+
+// const reveal = {
+//     origin : 'left-right',
+//     distance : '120px',
+//     easing   : 'ease-in-out',
+//   };
